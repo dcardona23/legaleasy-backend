972266b5badadef9b56457178564a9ae6ff57689
Danielle Cardona <danielle.cardona23@gmail.com>
2025-03-28 11:07:29 -0600

    implement session logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
